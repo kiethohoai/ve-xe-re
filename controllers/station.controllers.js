58e3dbd2cb356d04c6da9cb16271ea1e77d357c7
kiethohoai <kietho.060193@gmail.com>
2024-07-30 23:12:07 +0700

    Bài 9 Crud station read

--- controllers/station.controllers.js
+++ controllers/station.controllers.js
@@ -10,6 +10,28 @@ const createStation = async (req, res) => {
   }
 };
 
+const getAllStation = async (req, res) => {
+  try {
+    const stationList = await Station.findAll();
+    res.status(200).send(stationList);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getDetailStation = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const stationDetail = await Station.findOne({ where: { id } });
+    if (stationDetail) res.status(200).send(stationDetail);
+    else res.status(500).send('NOT FOUND!');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
 module.exports = {
   createStation,
+  getAllStation,
+  getDetailStation,
 };
--- routes/station.routes.js
+++ routes/station.routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { createStation } = require('../controllers/station.controllers');
+const {
+  createStation,
+  getAllStation,
+  getDetailStation,
+} = require('../controllers/station.controllers');
 
 const stationRouter = express.Router();
+
 stationRouter.post('/', createStation);
+stationRouter.get('/', getAllStation);
+stationRouter.get('/:id', getDetailStation);
 
 module.exports = {
   stationRouter,
